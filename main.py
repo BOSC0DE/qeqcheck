920ab73de4c7360da1f813882c46673916d84577
Mohammed <djeridam@uni-bremen.de>
2022-10-25 17:34:58 +0200

    miter construction and clause generation

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import ckt
 import argparse
 import readbench
 import adapter
+from lingeling import Solver
 
 
 def main(argv):
@@ -17,11 +18,24 @@ def main(argv):
     inputs_1, outputs_1, node_map_1 = readbench.readBenchFile(args.bench1)
     inputs_2, outputs_2, node_map_2 = readbench.readBenchFile(args.bench2)
 
-    assert outputs_2 == outputs_1
 
-    print (outputs_2)
-    print (outputs_1)
+    #match the outputs
+    for o1, o2 in zip(outputs_1, outputs_2):
+        assert o1.name == o2.name
 
+    miter = []
+    i = 0
+    for o1, o2 in zip(outputs_1, outputs_2):
+        miter.append(ckt.XorGate(o1, o2))
+        i += 1
+    out = ckt.OrGate(*miter)
+
+    node_to_lit_map = {}
+    S = Solver()
+    clauses = adapter.circuitToCNF(out, node_to_lit_map, lambda n: S.newVar())
+    print(clauses)
+    for n in node_to_lit_map:
+        print(n.name, node_to_lit_map[n])
 
 
 
